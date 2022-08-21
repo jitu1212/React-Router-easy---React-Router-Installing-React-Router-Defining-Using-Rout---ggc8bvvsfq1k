3f29496a8d0192197e61087d6317ad075ab8e927
jitu yadav <98046486+jitu1212@users.noreply.github.com>
2022-08-21 09:09:13 +0530

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 export const Home = () => {
     return (
-        <div className="home-page">Home Page</div>
-        
+        <div id="home-page">Home Page</div>
     )
 }
+
+export default Home
