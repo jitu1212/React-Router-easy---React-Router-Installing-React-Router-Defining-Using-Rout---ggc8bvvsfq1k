50112f1164107fa6fc6e8303477bba8abbd178a0
jitu yadav <98046486+jitu1212@users.noreply.github.com>
2022-08-21 09:47:42 +0530

    Update Index.js

--- src/Pages/Index.js
+++ src/Pages/Index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export const Index = () =>{
+export const Index = () => {
     return (
-        <div className="index-page">Index Page</div>
-     
+        <div id="index-page">Index Page</div>
     )
 }
-export default Index;
+
+export default Index
