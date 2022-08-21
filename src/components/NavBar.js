e1047d06b728554c1ce45af3365c324c1a0f3c47
jitu yadav <98046486+jitu1212@users.noreply.github.com>
2022-08-21 08:59:39 +0530

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Home } from '../Pages/Home';
-import { Index } from '../Pages/Index';
+import  Home from '../Pages/Home';
+import  Index from '../Pages/Index';
 
 import { NavLink } from 'react-router-dom';
 
